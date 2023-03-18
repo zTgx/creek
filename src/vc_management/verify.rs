afc342029066925d206ff1c020e73ed014c142df
zTgx <747674262@qq.com>
2023-03-18 13:51:25 +0800

    add vc_verify test

--- src/vc_management/verify.rs
+++ src/vc_management/verify.rs
@@ -1,5 +1,11 @@
 use jsonschema::{Draft, JSONSchema};
 
+/**
+ * Here are the key points that need to be verified for VC, and a large number of verifiable tests need to be conducted for VC.
+ * All test cases are processed centrally in `test_vc_verify.r`.
+ * 
+ * https://www.notion.so/web3builders/VC-Verification-e21fcb4cd9004672ac25b44609c8a86b
+ */
 pub fn verify_vc_schema(decrypt_vc: &[u8]) -> bool {
     let vc: serde_json::Value = serde_json::from_slice(decrypt_vc).unwrap();
     let schema = include_bytes!("../../docs/templates/vc_schema.json");
