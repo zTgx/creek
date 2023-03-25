1f8897b63a988623c157ed1ad4a987b0756ded8c
zTgx <747674262@qq.com>
2023-03-25 17:28:09 +0800

    assert context.is_verified

--- tests/test_corner_case.rs
+++ tests/test_corner_case.rs
@@ -664,6 +664,7 @@ fn tc_batch_all_create_more_than_100_identities_and_check_idgraph_size() {
 
     assert_eq!(decrypted_id_graph.len(), 20);
     decrypted_id_graph.iter().for_each(|(identity, context)| {
+        assert!(context.is_verified);
         assert!(all_created_identities[identites_len - 20..].contains(&identity));
 
         let identity_size = identity.encode().len();
