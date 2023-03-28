662327b385cf8350c6d9dcdc7c8c2d47e8932621
zTgx <747674262@qq.com>
2023-03-28 08:55:38 +0800

    add alpha function template

--- tests/test_alpha_launch.rs
+++ tests/test_alpha_launch.rs
@@ -2,5 +2,17 @@
 Put the alpha-launch testcase of litentry-parachain here
 Date: 2023-03-27
 Modules: IdentityManagament / VCManagement / Sidechain
- */
+function name template: alpha_000 ~ alpha_999
+*/
 
+/*
+Description:
+    XXX
+ */
+#[test]
+fn alpha_xxx() {
+    let left = 0;
+    let right = 0;
+
+    assert_eq!(left, right);
+}
