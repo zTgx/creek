306088c7941820d0c7923ce32a8273c3f77efd29
zTgx <747674262@qq.com>
2023-04-02 11:20:35 +0800

    using VCIndex instead

--- src/utils/vc.rs
+++ src/utils/vc.rs
@@ -1,6 +1,6 @@
-use sp_core::H256;
+use crate::primitives::VCIndex;
 
-pub fn create_a_random_vc_index() -> H256 {
+pub fn create_a_random_vc_index() -> VCIndex {
     let index: [u8; 32] = rand::random();
-    H256::from(index)
+    VCIndex::from(index)
 }
