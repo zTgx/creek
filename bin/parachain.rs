8bc0e07534f43c0c6b6c329ae14ef7a61a4a7ea2
zTgx <747674262@qq.com>
2024-01-05 10:09:54 +0000

    add backup data

--- bin/parachain.rs
+++ bin/parachain.rs
@@ -1,3 +1,11 @@
+use serde::{Deserialize, Serialize};
+use std::{
+	collections::HashMap,
+	fs::File,
+	io::{self, Write},
+	time::SystemTime,
+};
+
 use creek::{Creek, CreekExplorer, ParachainOp};
 use sp_core::{sr25519, Pair};
 
@@ -37,6 +45,37 @@ fn main() {
 	// 2: Set this alice as signer.
 	let creek = Creek::explorer(env.get_url().0, env.get_url().1, alice.into()).unwrap();
 
-	let contexts = creek.vc_registry().unwrap();
-	println!("VC contexts: {:#?}", contexts);
+	// 3. Query VCRegistry Data
+	let mut contexts = creek.vc_registry().unwrap();
+
+	// 4. Backup VCRegistry
+	let _ = contexts.write();
+}
+
+#[derive(Serialize, Deserialize)]
+struct Data {
+	map: HashMap<String, String>,
+}
+
+pub trait WriteToFile {
+	fn write(&mut self) -> io::Result<()>;
+}
+
+impl WriteToFile for HashMap<String, String> {
+	fn write(&mut self) -> io::Result<()> {
+		let timestamp = SystemTime::now()
+			.duration_since(SystemTime::UNIX_EPOCH)
+			.expect("Failed to retrieve timestamp")
+			.as_secs();
+
+		let data = Data { map: self.clone() };
+		let json = serde_json::to_string(&data)?;
+
+		let backup_path = "./bin/VCRegistryBackup";
+		let file_name = format!("{}/{}-vcregistry.json", backup_path, timestamp);
+		let mut file = File::create(file_name)?;
+		file.write_all(json.as_bytes())?;
+
+		Ok(())
+	}
 }
